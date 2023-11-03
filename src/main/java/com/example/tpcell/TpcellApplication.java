560d5adb9da861a2fd55efd5b0910cb1e59a7e98
somjena <jenasoumitra039@gmail.com>
2023-11-03 18:20:50 +0530

    created a class

---
+++ src/main/java/com/example/tpcell/TpcellApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpcell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpcellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpcell/TpcellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpcell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpcellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
