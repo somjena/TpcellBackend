e917241d1801ac0e46c6e9ebe8383274fb2f8a3a
Brijesh20000 <131857610+Brijesh20000@users.noreply.github.com>
2023-11-13 11:38:52 +0530

    create the Hr Repo and initialize the jpa Repository

---
+++ src/main/java/com/example/tpcell/Repository/HrRepository.java
@@ -0,0 +1,7 @@
+package com.example.tpcell.Repository;
+
+import com.example.tpcell.Entity.Hrs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HrRepository extends JpaRepository <Hrs , Long>{
+}
